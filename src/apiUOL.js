521e36c021a36b917dc23816d9413b871f9e01ef
Tarcísio F. S. Teles <tfst@poli.br / tfst@ecomp.poli.br>
2022-01-26 19:33:18 -0300

    test: testing 'get users'

--- src/apiUOL.js
+++ src/apiUOL.js
@@ -17,14 +17,35 @@ server.post('/participants', (req, res) => {
     console.log('connected to db ->', dbLink.s.url);
     const db = dbLink.db('apiUOL');
     const usersCollection = db.collection('users');
-    const newUserPromise = usersCollection.insertOne(req.body);
+    const newUserPromise = usersCollection.insertOne({name: req.body.name, lastStatus: Date.now() });
 
     newUserPromise.then(insertion => res.send(insertion));
     newUserPromise.catch(console.log);
-
   });
   
-  dbConnection.catch(console.log);
+  dbConnection.catch(err => {
+    console.log('error posting participant', err);
+    res.send(err);
+  });
+});
+
+server.get('/participants', (req, res) => {
+  const dbConnection = mongoClient.connect();
+
+  dbConnection.then( dbLink => {
+    const db = dbLink.db('apiUOL');
+    const usersCollection = db.collection('users');
+    const getUsersPromise = usersCollection.find().toArray();
+
+    getUsersPromise.then( search => res.send(search));
+    getUsersPromise.catch(console.log);
+  });
+
+
+  dbConnection.catch(err => {
+    console.log('error getting participants', err);
+    res.send(err);
+  });
 });
 
 const serverPort = 4000;
